109b0a40a914edcf58b8113d6eade5e4327d01d6
MilesAngels <mangels@alphaworks.tech>
2022-04-04 15:50:52 -0700

    making featured cards responsive

---
+++ js/responsive.js
@@ -0,0 +1,6 @@
+let hero_text = document.getElementsByClassName("hero.text");
+//console.log(hero_text.innerHTML);
+let hero_img = document.getElementsByClassName("hero-img");
+if (screen.width >= 768) {
+    //hero_text.classList.add("column");
+}
