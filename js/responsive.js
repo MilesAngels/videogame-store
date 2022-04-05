83fecde9649476f3649dc0792c9d19eb5dbb531c
MilesAngels <mangels@alphaworks.tech>
2022-04-05 11:46:57 -0700

    hover effect on grid

--- js/responsive.js
+++
@@ -1,6 +0,0 @@
-let hero_text = document.getElementsByClassName("hero.text");
-//console.log(hero_text.innerHTML);
-let hero_img = document.getElementsByClassName("hero-img");
-if (screen.width >= 768) {
-    //hero_text.classList.add("column");
-}
